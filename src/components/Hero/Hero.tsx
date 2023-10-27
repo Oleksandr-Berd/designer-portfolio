be3b4c6d0e760096f821adbda98ac05046a87f6b
Oleksandr-Berd <alex.berd86@gmail.com>
2023-10-27 07:39:08 +0300

    hero tablet is done

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,35 +1,70 @@
-import * as SC from "./HeroStyled"
+import { useMediaQuery } from "usehooks-ts";
 
-import {ReactComponent as HeroMobSvg} from "../../assets/images/hero_mob.svg"
+import * as SC from "./HeroStyled";
+
+import { ReactComponent as HeroMobSvg } from "../../assets/images/hero_mob.svg";
+import { ReactComponent as HeroTabletSvg } from "../../assets/images/hero_image_tablet.svg";
 
 const Hero = () => {
-    return (
-      <SC.CommonCon>
-        <SC.ImageCon>
-          <HeroMobSvg />
-        </SC.ImageCon>
-        <div>
-          <SC.Title>
-            I’m Amy, and I’d love to work on your next project
-          </SC.Title>
-          <SC.Paragraph>
-            I love working with others to create beautiful design solutions.
-            I’ve designed everything from brand illustrations to complete mobile
-            apps. I’m also handy with a camera!
-          </SC.Paragraph>
-        </div>
-        <SC.LinkCon>
-          <a
-            href="https://portfolio-aleks-berd.netlify.app/"
-            target="_blank"
-            rel="noreferrer"
-            style={{ textDecoration: "none" }}
-          >
-            Free Consultation
-          </a>
-        </SC.LinkCon>
-      </SC.CommonCon>
-    );
-}
- 
+  const isTablet = useMediaQuery("(min-width:768px)");
+
+  return (
+    <>
+      {isTablet ? (
+        <SC.CommonCon>
+          <SC.ImageCon>
+            <HeroTabletSvg />
+          </SC.ImageCon>
+          <SC.ContentCon>
+            <SC.Title>
+              I’m Amy, and I’d love to work on your next project
+            </SC.Title>
+            <SC.Paragraph>
+              I love working with others to create beautiful design solutions.
+              I’ve designed everything from brand illustrations to complete
+              mobile apps. I’m also handy with a camera!
+            </SC.Paragraph>
+            <SC.LinkCon>
+              <a
+                href="https://portfolio-aleks-berd.netlify.app/"
+                target="_blank"
+                rel="noreferrer"
+                style={{ textDecoration: "none" }}
+              >
+                Free Consultation
+              </a>
+            </SC.LinkCon>
+          </SC.ContentCon>
+        </SC.CommonCon>
+      ) : (
+        <SC.CommonCon>
+          <SC.ImageCon>
+            <HeroMobSvg />
+          </SC.ImageCon>
+          <div>
+            <SC.Title>
+              I’m Amy, and I’d love to work on your next project
+            </SC.Title>
+            <SC.Paragraph>
+              I love working with others to create beautiful design solutions.
+              I’ve designed everything from brand illustrations to complete
+              mobile apps. I’m also handy with a camera!
+            </SC.Paragraph>
+          </div>
+          <SC.LinkCon>
+            <a
+              href="https://portfolio-aleks-berd.netlify.app/"
+              target="_blank"
+              rel="noreferrer"
+              style={{ textDecoration: "none" }}
+            >
+              Free Consultation
+            </a>
+          </SC.LinkCon>
+        </SC.CommonCon>
+      )}
+    </>
+  );
+};
+
 export default Hero;
--- src/components/Hero/HeroStyled.ts
+++ src/components/Hero/HeroStyled.ts
@@ -6,10 +6,27 @@ export const CommonCon = styled.div`
   padding-bottom: 100px;
 
   text-align: center;
+
+  @media (min-width: 768px) {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+
+    padding-left: 0;
+  }
 `;
 
 export const ImageCon = styled.div`
   margin-bottom: 40px;
+
+  @media (min-width: 768px) {
+    margin-bottom: 0;
+    margin-right: 69px;
+  }
+`;
+
+export const ContentCon = styled.div`
+  text-align: left;
 `;
 
 export const Title = styled.h2`
@@ -18,6 +35,13 @@ export const Title = styled.h2`
   color: #030303;
   font-size: 26px;
   font-weight: bold;
+
+  @media (min-width: 768px) {
+    width: 319px;
+
+    font-size: 32px;
+    line-height: 1.25;
+  }
 `;
 
 export const Paragraph = styled.p`
@@ -26,10 +50,17 @@ export const Paragraph = styled.p`
   color: #7a746e;
   font-size: 16px;
   line-height: 1.63;
+
+  @media (min-width: 768px) {
+    width: 319px;
+
+    font-size: 18px;
+    line-height: 1.56;
+  }
 `;
 
 export const LinkCon = styled.div`
-display: inline-block;
+  display: inline-block;
 
   padding-left: 27px;
   padding-right: 28px;
@@ -40,10 +71,22 @@ display: inline-block;
 
   border-radius: 28px;
 
+  @media (min-width: 768px){
+    padding-left: 45px;
+    padding-right: 45px;
+    padding-top: 14px;
+    padding-bottom: 14px;
+  }
+
   & > a {
     color: #fff7f0;
     font-weight: bold;
     font-size: 14px;
     line-height: 2;
+
+    @media (min-width: 768px){
+      font-size: 16px;
+      line-height: 1.75;
+    }
   }
 `;
