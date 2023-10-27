3b1b51196c19fbbb0abfdad4a033277691fe0241
Oleksandr-Berd <alex.berd86@gmail.com>
2023-10-27 07:28:05 +0300

    services section tablet is done

--- src/components/MyServices/ServicesStyled.ts
+++ src/components/MyServices/ServicesStyled.ts
@@ -1,17 +1,37 @@
 import styled from "@emotion/styled";
 
 export const ServiceList = styled.ul`
-padding-left: 16px;
-padding-right: 16px;
-padding-bottom: 100px;
+  padding-left: 16px;
+  padding-right: 16px;
+  padding-bottom: 100px;
 
-& > li:not(:last-child){
+  & > li:not(:last-child) {
     margin-bottom: 24px;
-}
-`
+  }
+
+  @media (min-width: 768px) {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+
+    padding-bottom: 120px;
+  }
+`;
 
 export const ServicesItemDouble = styled.li`
 & > svg:not(:last-child){
     margin-right: 25px;
 }
+`
+
+export const ItemDoubleTablet = styled.div`
+margin-bottom: 24px;
+
+& > svg:not(:last-child){
+    margin-right: 23px;
+}
+`
+
+export const CombineItemTablet = styled.li`
+
+
 `
