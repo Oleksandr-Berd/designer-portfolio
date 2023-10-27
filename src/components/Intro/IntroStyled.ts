09beab99e99ce7c705c0be9f905c27ffcaa0e2c4
Oleksandr-Berd <alex.berd86@gmail.com>
2023-10-27 07:06:09 +0300

    intro tablet is done

--- src/components/Intro/IntroStyled.ts
+++ src/components/Intro/IntroStyled.ts
@@ -6,6 +6,12 @@ export const CommonCon = styled.div`
   padding-right: 16px;
 
   text-align: center;
+
+  @media (min-width: 768px) {
+    padding-bottom: 64px;
+    padding-left: 38px;
+    padding-right: 38px;
+  }
 `;
 
 export const Title = styled.h1`
@@ -14,6 +20,11 @@ export const Title = styled.h1`
   color: #030303;
   font-size: 36px;
   font-weight: bold;
+
+  @media (min-width: 768px) {
+    margin-bottom: 27px;
+    font-size: 44px;
+  }
 `;
 
 export const Paragraph = styled.p`
@@ -23,4 +34,12 @@ export const Paragraph = styled.p`
   color: #7a746e;
   font-size: 16px;
   line-height: 1.63;
+
+  @media (min-width: 768px) {
+    padding-left: 75px;
+    padding-right: 75px;
+
+    font-size: 18px;
+    line-height: 1.56;
+  }
 `;
